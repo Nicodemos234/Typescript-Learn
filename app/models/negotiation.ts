e4b5a0ead03e4d89db8055cdfeef7f263ac8011e
nicodemos234 <nicodemos54324@gmail.com>
2021-08-04 18:15:40 -0300

    protect the date attribute on negotiation model

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,11 +1,16 @@
 export class Negotiation {
     constructor(
-        public readonly date: Date,
+        private _date: Date,
         public readonly quantity: number,
         public readonly value: number
     ) { }
 
     get volume(): Number {
         return this.quantity * this.value;
+    }
+
+    get date(): Date {
+        const date = new Date(this.date.getTime());
+        return date;
     }
 }
