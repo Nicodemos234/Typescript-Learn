7e7471085c1a49cd9e5aae47379334df3f68a739
nicodemos234 <nicodemos54324@gmail.com>
2021-08-04 18:10:22 -0300

    refactor read only on attributs on negotation model

--- app/models/negotiation.ts
+++ app/models/negotiation.ts
@@ -1,23 +1,11 @@
 export class Negotiation {
     constructor(
-        private _date: Date,
-        private _quantity: number,
-        private _value: number
+        public readonly date: Date,
+        public readonly quantity: number,
+        public readonly value: number
     ) { }
 
-    get date(): Date {
-        return this._date;
-    }
-
-    get quantity(): Number {
-        return this._quantity;
-    }
-
-    get value(): Number {
-        return this._value;
-    }
-
     get volume(): Number {
-        return this._quantity * this._value;
+        return this.quantity * this.value;
     }
 }
